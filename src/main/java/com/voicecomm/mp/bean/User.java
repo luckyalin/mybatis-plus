360d16724748dfc4112c7b363faa2884379bb326
liyulin <liyulin@voicecomm.cn>
2021-04-08 14:10:13 +0800

    mybatis-plus第一个测试demo

---
+++ src/main/java/com/voicecomm/mp/bean/User.java
@@ -0,0 +1,17 @@
+package com.voicecomm.mp.bean;
+
+import lombok.Data;
+
+/**
+ * User
+ *
+ * @author 李钰林
+ * @date 2021/4/8
+ */
+@Data
+public class User {
+    private String id;
+    private String name;
+    private String age;
+    private String email;
+}
---
+++ src/main/java/com/voicecomm/mp/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.voicecomm.mp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.voicecomm.mp.bean.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * UserMapper
+ *
+ * @author 李钰林
+ * @date 2021/4/8
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
